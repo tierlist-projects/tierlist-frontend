efaae77d34cb5cd99958e9e01c0c380d7a44d03e
unzinzanda <seg412@naver.com>
2024-04-20 01:39:49 +0900

    feat(login): 로그인 구현
    - 이후 테스트 더 필요
    - 로그인 하면 유저 정보 얻어와서 저장하는 작업 등이 필요

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,10 +1,11 @@
-import { loginApi } from '@apis/login/loginApi'
 import { accessTokenState } from '@atom/userAtom'
 import { setCookie } from '@utils/cookie'
+import { http } from '@utils/http'
 import { useRef, useState } from 'react'
+import { ResponseTokenType } from 'types/auth.type'
 import { useSetRecoilState } from 'recoil'
 
-const useLogin = () => {
+const useLogin = (closeModal: () => void) => {
   const emailRef = useRef<HTMLInputElement>(null)
   const pwRef = useRef<HTMLInputElement>(null)
   const [errorMsg, setErrorMsg] = useState('')
@@ -18,22 +19,29 @@ const useLogin = () => {
         setErrorMsg('* 비밀번호를 입력해주세요.')
       } else {
         setErrorMsg('')
-        try {
-          const data = await loginApi({
+
+        http
+          .post(`login`, {
             email: emailRef.current.value,
             password: pwRef.current.value,
           })
+          .then((res) => {
+            const data = res as ResponseTokenType
 
-          setAccessToken(data.accessToken)
-          setCookie('refresh-token', `${data.tokenType} ${data.refreshToken}`, {
-            path: '/',
-            httpOnly: true,
+            setAccessToken(data.accessToken)
+            setCookie(
+              'refresh-token',
+              `${data.tokenType} ${data.refreshToken}`,
+              {
+                path: '/',
+                httpOnly: true,
+              },
+            )
+            closeModal()
           })
-
-          // 성공하면 모달 클로즈하는거 해줘야함
-        } catch (error) {
-          setErrorMsg('* 이메일과 비밀번호를 다시 확인해주세요.')
-        }
+          .catch(() => {
+            setErrorMsg('* 이메일과 비밀번호를 다시 확인해주세요.')
+          })
       }
     }
   }
