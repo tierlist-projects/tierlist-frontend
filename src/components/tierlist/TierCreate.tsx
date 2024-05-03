00b1cd12469e71d46650c2dd11b749ef3eebfe96
unzinzanda <seg412@naver.com>
2024-05-04 05:13:34 +0900

    feat(tierlist-create): 로그인해야 티어리스트 생성 모달 열리도록 함

--- src/components/tierlist/TierCreate.tsx
+++ src/components/tierlist/TierCreate.tsx
@@ -2,16 +2,27 @@ import React from 'react'
 import * as S from '@styles/tierlist/tierCreate.style'
 import { images } from '@constants/images'
 import useModal from '@hooks/useModal'
+import { useRecoilValue } from 'recoil'
+import { userState } from '@atom/userAtom'
 import CreateModal from './CreateModal'
 
 const TierCreate = () => {
   const { Modal, isOpen, openModal, closeModal } = useModal()
+  const user = useRecoilValue(userState)
   return (
     <>
-      <S.CreateButton type="button" onClick={openModal}>
+      <S.CreateButton
+        type="button"
+        onClick={() => {
+          if (user) openModal()
+          else {
+            alert('로그인을 해야 이용할 수 있습니다.')
+          }
+        }}
+      >
         <img src={images.common.plus} alt="게시글 작성" />
       </S.CreateButton>
-      <Modal isOpen={isOpen} closeModal={closeModal}>
+      <Modal isOpen={isOpen}>
         <CreateModal closeModal={closeModal} />
       </Modal>
     </>
