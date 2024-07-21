df556465113615bed48676012ac3af79463e9371
unzinzanda <seg412@naver.com>
2024-07-22 03:48:39 +0900

    feat(tierlist-read): PostCard 디자인 수정

--- src/styles/common/PostCard.style.ts
+++ src/styles/common/PostCard.style.ts
@@ -5,7 +5,7 @@ export const PostCardContainer = styled.div`
   width: 200px;
   height: 270px;
   border-radius: 15px;
-  box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0px 15px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
   align-items: center;
